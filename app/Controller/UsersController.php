aca065531eed8c5ccd33a768cb5fe9a1332bb987
Takuya Hasegawa <takuya@h-terry.com>
2015-11-25 20:18:53 +0900

    変更

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,35 @@
+<?php
+//
+class UsersController extends AppController {
+	public $components = array('Session','Auth');
+	public $uses = array('Post','User','Review');
+
+
+	public function index(){
+
+	}
+
+	public function register(){
+		if($this->request->is('post') && $this->User->save($this->request->data)){
+			$this->Auth->login();
+			$this->redirect('mypage/'.$this->Auth->user('id'));
+		}
+	}
+
+	public function login(){
+		if($this->request->is('post')){
+			if($this->Auth->login()){
+				$this->redirect('mypage/'.$this->Auth->user()['User']['id']);
+			}else{
+				$this->Session->setFlash('ログイン失敗');
+			}
+		}
+	}
+		
+	public function logout(){
+		$this->Auth->logout();
+		$this->redirect('login');
+	}
+}
+
+
